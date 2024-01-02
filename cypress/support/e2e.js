6f2877818976a6ced17a9c4173bc2b6f321aab35
MKefem <efe.mkadri@gmail.com>
2024-01-02 16:32:26 +0300

    Allure updated

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -22,41 +22,41 @@ import "cypress-mochawesome-reporter/register"
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
 Cypress.on('uncaught:exception', (err, runnable) => {
-  // returning false here prevents Cypress from
-  // failing the test
-  return false
+    // returning false here prevents Cypress from
+    // failing the test
+    return false
 })
-/*afterEach(() => {
-  const screenshotsFolder = Cypress.config('screenshotsFolder');
-  if (window.cucumberJson?.generate) {
-    const testState = window.testState;
-    const stepResult =
-      testState.runTests[testState.currentScenario.name][testState.currentStep];
-    if (stepResult?.status === 'failed') {
-      const screenshotFileName = `${testState.feature.name} -- ${testState.currentScenario.name} (failed).png`;
-      cy.readFile(
-        `${screenshotsFolder}/${Cypress.spec.name}/${screenshotFileName}`,
-        'base64',
-      ).then((imgData) => {
-        stepResult.attachment = {
-          data: imgData,
-          media: { type: 'image/png' },
-          index: testState.currentStep,
-          testCase: testState.formatTestCase(testState.currentScenario),
-        };
-      });
+afterEach(() => {
+    const screenshotsFolder = Cypress.config("screenshotsFolder");
+    if (window.cucumberJson?.generate) {
+        const testState = window.testState;
+        const stepResult =
+            testState.runTests[testState.currentScenario.name][testState.currentStep];
+        if (stepResult?.status === "failed" && !Cypress.config('isInteractive')) {
+            const screenshotFileName = `${testState.feature.name} -- ${testState.currentScenario.name} (failed).png`;
+            cy.readFile(
+                `${screenshotsFolder}/${Cypress.spec.relative.split('features/')[1]}/${screenshotFileName}`,
+                "base64"
+            ).then((imgData) => {
+                stepResult.attachment = {
+                    data: imgData,
+                    media: { type: "image/png" },
+                    index: testState.currentStep,
+                    testCase: testState.formatTestCase(testState.currentScenario),
+                };
+            });
+        }
     }
-  }
-}); */
+});
 
 // Hide fetch/XHR requests
-const app = window.top;
-if (!app.document.head.querySelector('[data-hide-command-log-request]')) {
-    const style = app.document.createElement('style');
-    style.innerHTML =
-        '.command-name-request, .command-name-xhr { display: none }';
-    style.setAttribute('data-hide-command-log-request', '');
+            const app = window.top;
+            if (!app.document.head.querySelector('[data-hide-command-log-request]')) {
+                const style = app.document.createElement('style');
+                style.innerHTML =
+                    '.command-name-request, .command-name-xhr { display: none }';
+                style.setAttribute('data-hide-command-log-request', '');
 
-    app.document.head.appendChild(style);
-}
+                app.document.head.appendChild(style);
+            }
 
